2298786e98ce3d048526b0fd3db89af02a1a763a
antony <anonymous@gmail.com>
2026-04-02 10:54:50 +0530

    (fix) product section font aligned

--- src/components/productdetailComponents/Banner.js
+++ src/components/productdetailComponents/Banner.js
@@ -5,7 +5,7 @@ import banner from "../../assets/banners/commonBanner.png";
 
 export default function ProductDetailComponent() {
     return (
-        <section className="relative w-full h-[260px] md:h-[360px] overflow-hidden">
+        <section className="relative w-full h-[320px] md:h-[480px]">
             {/* Background image */}
             <Image
                 src={banner}
@@ -24,7 +24,7 @@ export default function ProductDetailComponent() {
             />
 
             {/* Content */}
-            <div className="relative z-10 flex items-end justify-center h-full px-6">
+            <div className="absolute bottom-[-10px] md:bottom-[-20px] left-0 right-0 z-10 px-6 text-center">
                 <h2 className="text-[#F4E0C2] font-semibold">
                     Product Details
                 </h2>
